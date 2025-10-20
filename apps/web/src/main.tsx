a4ed710bcd9288efc7e24b96a14280ea7b8e2ecb
Sergiy Dybskiy <sergiy.dybskiy@sentry.io>
2025-10-20 14:47:57 -0400

    cleanup

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,4 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Note: Sentry will be added in Module 1 of the workshop
-// For now, keep it WITHOUT Sentry to show the "before" state
-
 createRoot(document.getElementById('root')!).render(<App />);
